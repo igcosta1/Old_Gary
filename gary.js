a131a49ace44f7bc4f566cc61746c0a5f1f8f020
igcosta1 <102997939+igcosta1@users.noreply.github.com>
2022-04-27 17:11:56 -0300

    Old_Gary

---
+++ gary.js
@@ -0,0 +1,9 @@
+function abrir(){
+    document.getElementById("chatGary").style.display = "block";
+    document.getElementById("gary").style.display = "none";
+}
+
+function fechar(){
+    document.getElementById("chatGary").style.display = "none";
+    document.getElementById("gary").style.display = "block";
+}
